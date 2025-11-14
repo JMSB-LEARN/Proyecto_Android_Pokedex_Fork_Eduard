c592dffc7dbb9dd7e90ae46e44a4de605a5b0b74
Abyss-2603 <eipp2603@gmail.com>
2025-11-14 18:59:46 +0100

    Generador_de_cartas

---
+++ app/src/main/java/es/iesagora/generador_de_cartas/detallesFragment.java
@@ -0,0 +1,20 @@
+package es.iesagora.generador_de_cartas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class detallesFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detalles, container, false);
+    }
+}
